3a28871a144abbb6a82491fd49d7bf5d737755bb
Zweihänder <zweidev@zweihander.me>
2021-05-01 16:28:57 -0400

    feat(kindle-highlights-to-org): add in processing
    Process file takes a file path and returns a hash table with all note
    values grouped by book title.
    
    Relies on the clippings file being consistently formatted.

--- kindle-highlights-to-org.el
+++ kindle-highlights-to-org.el
@@ -21,8 +21,9 @@ Return full path of the file. Performs basic checking on the file to confirm
 it can be read and the name is what the user intends."
   (interactive "P")
   (catch 'exit
-    (let ((full-file-path (or path
-                         (read-file-name "Select 'My Clippings.txt' file."))))
+    (let ((full-file-path (expand-file-name
+                           (or path
+                               (read-file-name "Select 'My Clippings.txt' file.")))))
       ;; Check filename, prompt if it seems wrong
       (unless (s-ends-with? "My Clippings.txt" full-file-path)
         (let ((proceed
@@ -40,9 +41,60 @@ it can be read and the name is what the user intends."
       ;; Return path
       full-file-path)))
 
+;;; Format notes:
+;;;     - 5 lines for each note, regardless of length
+;;; 1. TITLE
+;;; 2. METADATA
+;;;     - separated by |
+;;;     - sometimes 2 blocks, sometimes 3
+;;;     - last block should be time added
+;;; 3. BLANK LINE
+;;; 4. NOTEDATA
+;;;     - appears to always all be on one line
+;;;     - new lines are not preserved in any way
+;;; 5. SEPERATOR
+;;;     - 10 equal signs
+;;;     - ==========
+
+(defun kindle-highlights-to-org--process-file (path)
+  "Takes an absolute PATH and return a hash table with note data.
+Notes are grouped by the book each note came from.
+Format of returned hash table:
+KEY: TITLE
+VALUE:
+    List of plists, each with:
+        META: Metadata including location and highlight date as string.
+        NOTE: Notedata as one string (newlines aren't saved by the device)."
+  (let ((bookhash (make-hash-table :test 'equal)))
+    (with-temp-buffer
+      (insert-file-contents path)
+      (while (search-forward "==========" nil t)
+        (forward-line -4) ; to title
+        (let* ((title (buffer-substring-no-properties
+                       (line-beginning-position)
+                       (line-end-position)))
+               (metadata (progn (forward-line 1)
+                                (buffer-substring-no-properties
+                                 (line-beginning-position)
+                                 (line-end-position))))
+               (notedata (progn (forward-line 2)
+                                (buffer-substring-no-properties
+                                 (line-beginning-position)
+                                 (line-end-position)))))
+          ;; TODO break up metadata
+          (unless (gethash title bookhash)
+            (puthash title '() bookhash))
+          (let ((curnotes (gethash title bookhash))
+                (note-plist (list :meta metadata
+                                  :note notedata)))
+            (push note-plist curnotes)
+            (puthash title curnotes bookhash))
+          (forward-line 1)
+          (end-of-line))) ; end of note block
+      bookhash)))
+
 (defun kindle-highlights-to-org--main ()
   "Placeholder."
-  ;; Reads the file into a variable
   ;; Parses the file into note blocks
   ;; Creates a books set-like list
   ;; Loops through blocks
