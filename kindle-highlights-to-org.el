030fc24850cd54ae1ff64fab8c65e8d37836e39a
Zweihänder <zweidev@zweihander.me>
2021-05-01 00:21:19 -0400

    feat(kindle-highlights-to-org): add in file prompt
    Asks for file name, checks if it ends with likely Kindle name, and if
    not, ask the user to confirm.
    
    Could probably use some more checks but this will do for now.

--- kindle-highlights-to-org.el
+++ kindle-highlights-to-org.el
@@ -11,19 +11,35 @@
 ;;
 ;;; Code:
 
-;; Make sure running in org buffer
+;; TODO: Make sure running in org buffer
 
-;; Asks the user for a file to read
-;; Reads the file into a variable
-;; Parses the file into note blocks
-;; Creates a books set-like list
-;; Loops through blocks
-;; Parses metadata and note data from each block
-;; If the book doesn't exist in the book list, add it
-;; Add block data to book in book list
-;; For each book in book list, create a book org heading
-;; Under each book org heading, create a note heading for each note
-;; Add in note metadata under each note
+(require 's)
+
+(defun kindle-highlights-to-org--main ()
+  "Placeholder."
+  ;; Asks the user for a file to read
+  (interactive)
+  (catch 'exit
+    (let ((file-name (read-file-name "Select 'My Clippings.txt' file.")))
+      ;; Check filename, prompt if it seems wrong
+      (unless (s-ends-with? "My Clippings.txt" file-name)
+        (let ((proceed
+              (y-or-n-p (concat "File doesn't match 'My Clippings.txt'."
+                                "\n"
+                                "are you sure this is the right file? "))))
+          (unless proceed
+            (throw 'exit "Not the right file, cancelling."))))))
+  ;; Reads the file into a variable
+  ;; Parses the file into note blocks
+  ;; Creates a books set-like list
+  ;; Loops through blocks
+  ;; Parses metadata and note data from each block
+  ;; If the book doesn't exist in the book list, add it
+  ;; Add block data to book in book list
+  ;; For each book in book list, create a book org heading
+  ;; Under each book org heading, create a note heading for each note
+  ;; Add in note metadata under each note
+  )
 
 (provide 'kindle-highlights-to-org)
 
